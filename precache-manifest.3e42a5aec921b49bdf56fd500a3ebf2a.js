fe141627fbbeb749c67bf855b9afef9b2a64f2b5
developervilchez <developervilchez@gmail.com>
2018-10-10 14:59:00 -0500

    Updates

---
+++ precache-manifest.3e42a5aec921b49bdf56fd500a3ebf2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dcd9240152de926c6d1",
+    "url": "/my-calc-app/static/js/runtime~main.4dcd9240.js"
+  },
+  {
+    "revision": "d527acf7673ba5d7e60a",
+    "url": "/my-calc-app/static/js/main.d527acf7.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/my-calc-app/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "d527acf7673ba5d7e60a",
+    "url": "/my-calc-app/static/css/main.ba4b5dcd.chunk.css"
+  },
+  {
+    "revision": "4a3ddbe5c90c4b5fc8c84e6e653292ec",
+    "url": "/my-calc-app/index.html"
+  }
+];
